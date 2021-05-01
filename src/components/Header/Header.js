a9dbf1172829e27724d94d08a64df192e7594d83
md umar faruk <mdumar11233>
2021-05-01 11:15:49 +0600

    added new code

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,8 +5,8 @@ import './Header.css';
 const Header = () => {
     return (
         <div className="header-container">
-            <div className='container'>
-                <Navbar expand="lg" style={{height: '65px'}}>
+            <div>
+                <Navbar expand="lg" style={{height: '65px', color: ''}}>
                 <Navbar.Brand style={{fontSize: '30px', fontWeight: 'bold', color: '#fff'}} href="/home">U</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
@@ -16,7 +16,7 @@ const Header = () => {
                         <Nav.Link className='menu-link' href="/work">Work</Nav.Link>
                         <Nav.Link className='menu-link' href="/blog">Blog</Nav.Link>
                         <Nav.Link className='menu-link'  href="/contact">Contact</Nav.Link>
-                        <Nav.Link className='menu-link resume-btn'  href="/contact">Resume</Nav.Link>
+                        <Nav.Link className='resume-btn'  href="/contact">Resume</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
                 </Navbar>
--- src/components/TopBaner/TopBaner.js
+++ src/components/TopBaner/TopBaner.js
@@ -6,14 +6,15 @@ import './TopBaner.css';
 const TopBaner = () => {
     return (
         <div className="top-baner">
-                    <div className='container'>
-            <div className="row">
+            <div className="overlay"></div>
+            <div className='container'>
+            <div className="row text-center">
                 <div className="col-md-6 my-title">
-                    <h1>I'm Md Umar Faruk</h1>
-                    <div>
+                    <h1><span style={{color: '#FF0077'}}>Hi,</span> I'm Md Umar Faruk</h1>
+                    <div className='mt-4'>
                         <ReactTypingEffect
                             text={["Web Developer", "Programmer"]}
-                            cursorRenderer={cursor => <h4>{cursor}</h4>}
+                            cursorRenderer={cursor => <h4 style={{color: '#fff'}}>{cursor}</h4>}
                             displayTextRenderer={(text, i) => {
                             return (
                                 <h4>
@@ -22,6 +23,7 @@ const TopBaner = () => {
                                     return (
                                     <span
                                         key={key}
+                                        style={{color: '#fff', fontFamily: "'Lora', serif"}}
                                     >{char}</span>
                                     );
                                 })}
@@ -30,12 +32,13 @@ const TopBaner = () => {
                             }}        
                         />
                     </div>
-                    <div>
+                    <div className='mt-4'>
                         <button className='about-me me-3'>About me</button>
                         <span><button className='get-in-touch'>Get in Touch</button></span>
                     </div>
                 </div>
                 <div className="col-md-6 my-img">
+                    <div className="outline"></div>
                     <img src={picture} alt=""/>
                 </div>
             </div>
