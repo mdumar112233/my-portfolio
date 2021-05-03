9da247a0ef1869e10163d0f38b0c10e5bc792502
md umar faruk <mdumar11233>
2021-05-03 15:31:09 +0600

    added new code

--- src/components/TopBaner/TopBaner.js
+++ src/components/TopBaner/TopBaner.js
@@ -11,7 +11,7 @@ const TopBaner = () => {
         <div className="top-baner">
             <div className="overlay"></div>
             <div className='container'>
-            <div className="row text-center">
+            <div className="row flex-column-reverse flex-md-row text-center">
                 <div className="col-md-6 my-title">
                     <h1><span style={{color: '#FF0077'}}>Hi,</span> I'm Md Umar Faruk</h1>
                     <div className='mt-4'>
@@ -40,15 +40,15 @@ const TopBaner = () => {
                         <span><a rel="noreferrer" href='https://drive.google.com/uc?export=download&id=17z45BnAgosSL1GyXQHLQ3-IRxh-zksdq' className='get-in-touch'>Download CV</a></span>
                     </div>
                 </div>
-                <div className="col-md  my-img">
-                    <div className="outline"></div>
+                <div className="col-md-6  my-img">
+                    {/* <div className="outline"></div> */}
                     <img src={picture} alt=""/>
                 </div>
                 <div className='social text-center mt-5 social-icon'>
                         <a target="_blank" rel="noreferrer" href="https://www.facebook.com/mdumar.faruk.3139241"><img src={facebook} alt=""/></a>
                         <span><a target="_blank" rel="noreferrer" href="https://github.com/mdumar112233"><img src={github} alt=""/></a></span>
                         <span><a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/umar-faruk-89a480185/"><img src={linkedin} alt=""/></a></span>
-            </div>
+                </div>
             </div>
         </div>
         </div>
